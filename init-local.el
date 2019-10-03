fa6c6708f7119ed2d847674f2f5a182099b6b377
Senhui Guo <alex2014@mail.ustc.edu.cn>
2019-10-03 10:59:39 +1000

    update

--- init-local.el
+++ init-local.el
@@ -1,21 +1,59 @@
+;;; Commentary:
+
+;; My personal settings:
+
+;;; Code:
+
+;; My preferred packages
+(require 'nav)
+(require 'undo-tree)
+(require 'jedi)
+(require 'google-c-style)
+
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
+
+;; better experience
+(setq ns-pop-up-frames nil)
+                                        ; double click to open emacs in the existing process
+(ido-mode t)
+                                        ; better command buffer
+(set-default-font "Source Code Pro")
+                                        ; font
+
+
+;; python flycheck
+(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook (lambda () (setq display-line-numbers 'relative))) 
+(require 'python-environment)
+(setq jedi:environment-root "jedi")
+; (setq jedi:environment-virtualenv
+;     (append python-environment-virtualenv
+;         '("--python" "python3")))
+; (setq jedi:server-args
+;     '("--sys-path" "/usr/local/lib/python3.7/site-packages"))
+(setq jedi:complete-on-dot t)
+
+(setq multi-term-program-switches "--login")
+
+(add-hook 'term-mode-hook (lambda ()
+                            (define-key term-raw-map (kbd "C-y") 'term-paste)
+                            (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+                            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
+                            (setq term-buffer-maximum-size 0)))
+
+
+
+;; editor settings
+;;(global-linum-mode t)
+
+;;keybinding settings
+;;(global-set-key (kbd "M-h") 'mark-paragraph)
+(global-set-key (kbd "M-`") 'next-multiframe-window)
+
+;; (global-set-key (kbd "M-[") 'previous-buffer)
+;; (global-set-key (kbd "M-]") 'next-buffer)
+;; use C-x left/right to navigate through buffers instead
+
 (provide 'init-local)
-
-;;settings for R
-(setenv "LC_ALL" "en_US.UTF-8")
-
-;;add XeLaTeX to the TeX command list
-;;(add-hook 'LaTeX-mode-hook 
-;;          (lambda()
-;;             (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
-;;             (setq TeX-command-default "XeLaTeX")
-;;             (setq TeX-save-query nil)
-;;             (setq TeX-show-compilation t)))
-
-;;set up python interpreter
-(setq python-shell-interpreter "ipython3")
-(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
-
-;;set up ein mode
-(require 'ein)
-;;set delete cell key
-;;(define-key ein:notebook-mode-map "\C-c\C-d" 'ein:worksheet-delete-cell)
